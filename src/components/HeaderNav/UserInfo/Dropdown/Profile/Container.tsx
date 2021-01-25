85c316f77ce5e99a5c52ffc8303e059223921f36
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-01-25 18:08:30 +0900

    fix: 型定義修正

--- src/components/HeaderNav/UserInfo/Dropdown/Profile/Container.tsx
+++ src/components/HeaderNav/UserInfo/Dropdown/Profile/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Component, ComponentProps} from './Component';
 
-export type ContainerProps = ComponentProps;
+export type ContainerProps = Omit<ComponentProps, 'link'>;
 export const Container: React.FC<ContainerProps> = ({...props}) => {
   return <Component {...props} link={`/users/${props.userName}`} />;
 };
