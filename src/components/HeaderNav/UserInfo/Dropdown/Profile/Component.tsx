0e46ba6899d74663a2c857a239a7c3d6be0415cd
SnO₂WMaN <sno2wman.official@gmail.com>
2021-01-25 18:16:44 +0900

    refactor: HeaderNavのリファクタリング (#7)

--- src/components/HeaderNav/UserInfo/Dropdown/Profile/Component.tsx
+++ src/components/HeaderNav/UserInfo/Dropdown/Profile/Component.tsx
@@ -1,30 +1,25 @@
 import clsx from 'clsx';
-import NextImage from 'next/image';
 import NextLink from 'next/link';
 import React from 'react';
+import {Icon} from '~/components/common/Icon';
 
 export type ComponentProps = {
   className?: string;
+  link: string;
   picture: string;
   userName: string;
   displayName: string;
 };
 export const Component: React.FC<ComponentProps> = ({
   className,
+  link,
   picture,
   userName,
   displayName,
 }) => (
-  <NextLink href="/profile">
+  <NextLink href={link}>
     <a className={clsx(className, 'flex', 'py-2', 'px-4', 'hover:bg-blue-50')}>
-      <div className={clsx('mr-4', 'relative', 'w-12', 'h-12')}>
-        <NextImage
-          className={clsx('rounded-full')}
-          src={picture}
-          width={64}
-          height={64}
-        />
-      </div>
+      <Icon className={clsx('mr-4', 'w-12', 'h-12')} picture={picture} />
       <div className={clsx('flex', 'flex-col', 'justify-center', 'w-40')}>
         <span className={clsx('font-bold', 'select-all', 'truncate')}>
           {displayName}
--- src/components/HeaderNav/UserInfo/Dropdown/Profile/Container.tsx
+++ src/components/HeaderNav/UserInfo/Dropdown/Profile/Container.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Component, ComponentProps} from './Component';
 
-export type ContainerProps = ComponentProps;
+export type ContainerProps = Omit<ComponentProps, 'link'>;
 export const Container: React.FC<ContainerProps> = ({...props}) => {
-  return <Component {...props} />;
+  return <Component {...props} link={`/users/${props.userName}`} />;
 };
 Container.displayName = 'Profile';
