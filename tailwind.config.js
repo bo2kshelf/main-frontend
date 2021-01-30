bbd7b856e8ce8aba4b8c9f427794843d2727ccd5
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-01-30 16:02:32 +0900

    chore: tailwindにGitHub,Twitter,Googleのブランドカラー追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
   },
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        github: {
+          1: '#24292e',
+        },
+        twitter: {
+          1: '#1da1f2',
+        },
+        google: {
+          1: '#4285f4',
+          2: '#34a853',
+          3: '#fbbc05',
+          4: '#ea4335',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
