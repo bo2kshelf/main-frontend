0f0c1d88d87e2531f2b8efb1d670049c4b48c12a
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-02-13 15:06:36 +0900

    feat: UserPageをBooksSectionに切り出し

--- src/templates/server/UserPage/RecordsSection/Component.tsx
+++ src/templates/server/UserPage/RecordsSection/Component.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {useTranslation} from 'react-i18next';
 import {BaseComponent} from './BaseComponent';
 import {
   HaveHeader,
@@ -8,88 +7,40 @@ import {
   ReadingHeader,
   StackedHeader,
 } from './Header';
+import {
+  HaveRecordsNoBox,
+  ReadingRecordsNoBox,
+  ReadRecordsNoBox,
+  StackedRecordsNoBox,
+} from './NoRecordBox';
 
 export type ComponentProps = {
   className?: string;
   user: HeaderProps['user'];
-  records: {
-    book: {id: string; title: string; cover?: string};
-  }[];
+  records: {book: {id: string; title: string; cover?: string}}[];
   hasNext: boolean;
 };
 
-export const ReadRecordsSection: React.FC<ComponentProps> = ({
-  user,
-  ...props
-}) => {
-  const {t} = useTranslation();
-  return (
-    <BaseComponent
-      {...props}
-      user={user}
-      Header={ReadHeader}
-      i18n={{
-        noRecord: t('{{name}}はまだ読んだ本を登録していません', {
-          name: user.displayName,
-        }),
-      }}
-    />
-  );
-};
+export const ReadRecordsSection: React.FC<ComponentProps> = ({...props}) => (
+  <BaseComponent {...props} Header={ReadHeader} NoBooksBox={ReadRecordsNoBox} />
+);
 
-export const ReadingRecordsSection: React.FC<ComponentProps> = ({
-  user,
-  ...props
-}) => {
-  const {t} = useTranslation();
-  return (
-    <BaseComponent
-      {...props}
-      Header={ReadingHeader}
-      user={user}
-      i18n={{
-        noRecord: t('{{name}}が読んでいる本は今ありません', {
-          name: user.displayName,
-        }),
-      }}
-    />
-  );
-};
+export const ReadingRecordsSection: React.FC<ComponentProps> = ({...props}) => (
+  <BaseComponent
+    {...props}
+    Header={ReadingHeader}
+    NoBooksBox={ReadingRecordsNoBox}
+  />
+);
 
-export const HaveRecordsSection: React.FC<ComponentProps> = ({
-  user,
-  ...props
-}) => {
-  const {t} = useTranslation();
-  return (
-    <BaseComponent
-      {...props}
-      Header={HaveHeader}
-      user={user}
-      i18n={{
-        noRecord: t('{{name}}はまだ持っている本を登録していません', {
-          name: user.displayName,
-        }),
-      }}
-    />
-  );
-};
+export const HaveRecordsSection: React.FC<ComponentProps> = ({...props}) => (
+  <BaseComponent {...props} Header={HaveHeader} NoBooksBox={HaveRecordsNoBox} />
+);
 
-export const StackedRecordsSection: React.FC<ComponentProps> = ({
-  user,
-  ...props
-}) => {
-  const {t} = useTranslation();
-  return (
-    <BaseComponent
-      {...props}
-      Header={StackedHeader}
-      user={user}
-      i18n={{
-        noRecord: t('{{name}}が積んでいる本は今ありません', {
-          name: user.displayName,
-        }),
-      }}
-    />
-  );
-};
+export const StackedRecordsSection: React.FC<ComponentProps> = ({...props}) => (
+  <BaseComponent
+    {...props}
+    Header={StackedHeader}
+    NoBooksBox={StackedRecordsNoBox}
+  />
+);
---
+++ src/templates/server/UserPage/RecordsSection/NoRecordBox/index.ts
@@ -0,0 +1,4 @@
+export * from './BaseComponent';
+export type {BaseComponentProps} from './BaseComponent';
+export * from './Component';
+export type {ComponentProps as NoRecordsBoxProps} from './Component';
