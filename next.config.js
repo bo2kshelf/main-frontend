f1ff82af9ca31c433a4653c1bfcc27349e622f8e
SnO2WMaN <me@sno2wman.net>
2021-03-20 08:56:14 +0900

    feat: デプロイ先で利用するアクセストークン

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   env: {
+    AUTH_SERVER_ENDPOINT: process.env.AUTH_SERVER_ENDPOINT,
     GRAPHQL_API_ENDPOINT: process.env.GRAPHQL_API_ENDPOINT,
+    GRAPHQL_API_SERVER_ACCESS_TOKEN:
+      process.env.GRAPHQL_API_SERVER_ACCESS_TOKEN,
   },
   images: {
     domains: [process.env.IMAGE_PROXY_HOST],
--- src/lib/GraphQLRequest.ts
+++ src/lib/GraphQLRequest.ts
@@ -5,5 +5,9 @@ import {getSdk} from '~/_generated/graphql-request';
 export const graphqlSdk = getSdk(
   new GraphQLClient(graphqlConfig.externalEndpoint, {
     credentials: 'include',
+    headers: {
+      // eslint-disable-next-line no-process-env
+      Authorization: `Bearer ${process.env.GRAPHQL_API_SERVER_ACCESS_TOKEN}`,
+    },
   }),
 );
