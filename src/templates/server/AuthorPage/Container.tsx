1abc75d2fd35955ac6a04027207fc9893c5de67d
SnO2WMaN <me@sno2wman.net>
2021-04-06 05:14:05 +0900

    feat: ページタイトル

--- src/templates/server/AuthorPage/Container.tsx
+++ src/templates/server/AuthorPage/Container.tsx
@@ -1,9 +1,18 @@
+import Head from 'next/head';
 import React from 'react';
+import {useTranslation} from 'react-i18next';
 import {Component} from './Component';
 import {TransformedProps} from './transform';
 
 export type ContainerProps = TransformedProps;
-export const Container: React.FC<ContainerProps> = ({...props}) => {
-  return <Component {...props} />;
+export const Container: React.FC<ContainerProps> = (props) => {
+  const {t} = useTranslation();
+  return (
+    <Component {...props}>
+      <Head>
+        <title>{t('head:author_page', {name: props.name})}</title>
+      </Head>
+    </Component>
+  );
 };
 Container.displayName = 'AuthorPage';
--- src/templates/server/UserPage/Container.tsx
+++ src/templates/server/UserPage/Container.tsx
@@ -1,9 +1,19 @@
+import Head from 'next/head';
 import React from 'react';
+import {useTranslation} from 'react-i18next';
 import {Component} from './Component';
 import {TransformedProps} from './transform';
 
 export type ContainerProps = TransformedProps;
-export const Container: React.FC<ContainerProps> = ({...props}) => {
-  return <Component {...props} />;
+export const Container: React.FC<ContainerProps> = (props) => {
+  const {t} = useTranslation();
+
+  return (
+    <Component {...props}>
+      <Head>
+        <title>{t('head:user_page', {name: props.displayName})}</title>
+      </Head>
+    </Component>
+  );
 };
 Container.displayName = 'UserPage';
