12295acc1ff1de977646506e6fa7b23ef18f99df
SnO2WMaN <me@sno2wman.net>
2021-05-08 12:23:35 +0900

    feat: クエリ更新

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,13 +1,16 @@
 import {NextPage} from 'next';
 import React from 'react';
-import {usePersonalUserPageQuery} from '~/graphql/api-authenticated/codegen/apollo';
+import {useCurrentUserPageQuery} from '~/graphql/api-authenticated/codegen/apollo';
 import {withPageLoggedIn} from '~/lib/withPageLoggedIn';
 import {LoadingPage} from '~/templates/Loading';
 import {PersonalUserPage, transform} from '~/templates/PersonalUser';
 
 export const Page: NextPage = (props) => {
-  const {data, loading, error} = usePersonalUserPageQuery();
-  if (data) return <PersonalUserPage {...transform(data)} />;
+  const {data, loading, error} = useCurrentUserPageQuery();
+  if (data)
+    return (
+      <PersonalUserPage {...transform(data, {recordLimit: 5, recordSkip: 0})} />
+    );
   if (loading) return <LoadingPage />;
   return <div />;
 };
