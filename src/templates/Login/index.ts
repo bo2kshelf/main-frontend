cc9ca542b662da3fbb3422fd799d9b133bcb1853
SnO2WMaN <me@sno2wman.net>
2021-05-10 19:45:01 +0900

    feat: Login templates削除

--- src/templates/Login/index.ts
+++
@@ -1,4 +0,0 @@
-// export {Component} from './Component';
-// export type {ComponentProps} from './Component';
-export {Container as LoginPage} from './Container';
-export type {ContainerProps as LoginPageProps} from './Container';
